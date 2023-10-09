c50b309727df319446fbb31a15b6a8f65af7b7ef
JkrCode <40894524+JkrCode@users.noreply.github.com>
2023-10-09 14:41:14 +0200

    added posts to be displayed at landing page

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = 'http://localhost:3000/posts';
+
+    axios.get(apiUrl)
+      .then(response => {
+        setPosts(response.data.posts);
+      })
+      .catch(error => {
+        console.error('Fehler beim Laden der Daten:', error);
+      });
+  }, []);
+
+  const renderedPosts = posts.map((post, index) => (
+      <li key={index}>{post.text}</li>
+  ))
+
   return (
     <div>
-        <p>
-            from Home
-        </p>
+      <h1>Posts</h1>
+      <ul>
+        {renderedPosts}
+      </ul>
     </div>
   );
 }
